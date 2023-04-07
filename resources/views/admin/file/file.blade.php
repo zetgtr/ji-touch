28c04ceda45f543a935088b0c3a2fa83eab729e8
zetgtr <artem.kudrin70@gmail.com>
2023-04-08 02:20:38 +0300

    Артем
    Наконец то починил файловый менеджер

--- resources/views/admin/file/file.blade.php
+++ resources/views/admin/file/file.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.admin')
 @section('title',"Файлы")
 @section('content')
-    <iframe src="/laravel-filemanager" style="width: 100%; height: 80vh; overflow: hidden; border: none;"></iframe>
+    <iframe src="/laravel-filemanager?type=image" style="width: 100%; height: 80vh; overflow: hidden; border: none;"></iframe>
 @endsection
