1f8bf1158f2061eb7013bc8556c138d95419b42c
zetgtr <artem.kudrin70@gmail.com>
2023-04-14 12:04:03 +0300

    Артем
    Починил навигацию

--- resources/views/components/admin/navigation/link.blade.php
+++ resources/views/components/admin/navigation/link.blade.php
@@ -1,14 +1,18 @@
-<ol class="dd-list">
-    @forelse($link as $item)
-        <li class="dd-item delete-element" data-id="{{$item->id}}" data-name="{{$item->title}}">
-            <div class="dd-handle">
-                <span>{{$item->title}}</span>
-            </div>
-            @if($item->parent instanceof \Illuminate\Database\Eloquent\Collection)
-                <x-admin.navigation.link :link="$item->parent" />
-            @endif
-        </li>
-    @empty
-        <li class="dd-empty"></li>
-    @endforelse
-</ol>
+@if(count($link) > 0)
+    <ol class="dd-list">
+        @foreach($link as $item)
+            <li class="dd-item delete-element" data-id="{{$item->id}}" data-name="{{$item->title}}">
+                <div class="dd-handle">
+                    <span>{{$item->title}}</span>
+                </div>
+                @if($item->parent instanceof \Illuminate\Database\Eloquent\Collection)
+                    <x-admin.navigation.link :link="$item->parent" />
+                @endif
+            </li>
+        @endforeach
+    </ol>
+@endif
+
+@if(count($link) == 0)
+    <div class="dd-empty"></div>
+@endif
